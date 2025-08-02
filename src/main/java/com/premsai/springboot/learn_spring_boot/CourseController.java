6571e98a1677b746ffbd7faba03662e64bccbd9a
premsai-20 <sakamurisai252@gmail.com>
2025-08-02 16:12:26 +0530

    Initial commit

---
+++ src/main/java/com/premsai/springboot/learn_spring_boot/CourseController.java
@@ -0,0 +1,20 @@
+package com.premsai.springboot.learn_spring_boot;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CourseController {
+	
+	@RequestMapping("/courses")
+	public List<Course> retrieveAllCourses(){
+		return Arrays.asList(
+				new Course(1, "Learn AWS","Prof. FangYuan"),
+				new Course(2, "Learn DevOps","Prof. Bai Ning Bing")
+				);
+	}
+	
+}
